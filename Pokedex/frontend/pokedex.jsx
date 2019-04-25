3d275bc20342075ecfb5a1c7db193dbc0ac55638
grantpaulson6 <grantpaulson6@gmail.com>
2019-04-24 17:06:14 -0700

    Complete redux cycle for getting all pokemon

---
+++ Pokedex/frontend/pokedex.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store'
+import Root from './components/root'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById('root')
+
+    const store = configureStore();
+    window.getState = store.getState
+    window.dispatch = store.dispatch
+
+    ReactDOM.render(<Root/>, rootEl);
+});
